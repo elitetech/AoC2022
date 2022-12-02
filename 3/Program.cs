acb8919c0ba60b96290c0c710d332993f4ec5cef
Koty Bashford <koty.bashford@gmail.com>
2022-12-02 09:33:23 -0500

    Day 3 setup

---
+++ 3/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace AoC
+{
+    class AoC
+    {
+        static void Main()
+        {
+            var sample = System.IO.File.ReadAllLines("sample.txt");
+            var p1 = Part1(sample);
+            var p2 = Part2(sample);
+            Console.WriteLine($"Part 1: {p1}");
+            Console.WriteLine($"Part 2: {p2}");
+        }
+
+        public static int Part1(string[] sample)
+        {
+            int total = 0;
+            
+            return total;
+        }
+
+        public static int Part2(string[] sample)
+        {
+            int total = 0;
+            
+            
+            return total;
+        }
+    }
+}
