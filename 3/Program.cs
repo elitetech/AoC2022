93e767e3850c168cedd0b1a7215606055e0aa305
Koty Bashford <koty.bashford@gmail.com>
2022-12-03 01:27:17 -0500

    Day 3

--- 3/Program.cs
+++ 3/Program.cs
@@ -14,16 +14,45 @@
         public static int Part1(string[] sample)
         {
             int total = 0;
-            
+            foreach(var line in sample)
+            {
+                var sack = line.ToCharArray();
+                var compartmentOne = sack.Take(sack.Length / 2).ToArray();
+                var compartmentTwo = sack.Skip(sack.Length / 2).ToArray();
+                var common = compartmentOne.Intersect(compartmentTwo).ToArray();
+                total += GetPriority(common[0]);
+            }
             return total;
         }
 
         public static int Part2(string[] sample)
         {
             int total = 0;
-            
-            
+            List<List<char[]>> groups = new();
+            List<char[]> group = new();
+            foreach(var line in sample)
+            {
+                var sack = line.ToCharArray();
+                group.Add(sack);
+                if(group.Count() == 3)
+                {
+                    groups.Add(group);
+                    var common = group[0].Intersect(group[1]).Intersect(group[2]).ToArray();
+                    total += GetPriority(common[0]);
+                    group = new();
+                }
+            }
             return total;
+        }
+
+        public static int GetPriority(char character)
+        {
+
+            int index = (int)character % 32;
+
+            index += char.IsUpper(character) ? 26 : 0;
+
+            return index;
         }
     }
 }
