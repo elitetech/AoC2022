bd79cd3f9d19d4e2dedd1118c0a89fdf3c63896b
Koty Bashford <koty.bashford@gmail.com>
2022-12-06 10:31:10 -0500

    Day 6

---
+++ 6/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace AoC2022_6
+{
+    class AoC2022_6
+    {
+        public static void Main()
+        {
+            var sample = File.ReadAllText("sample.txt");
+
+            System.Console.WriteLine($"Part 1: {Part1(sample)}");
+            System.Console.WriteLine($"Part 2: {Part2(sample)}");
+        }
+
+        public static int Part1(string sample)
+        {
+            return GetStartOf(sample, 4);
+        }
+
+        public static int Part2(string sample)
+        {
+            return GetStartOf(sample, 14);
+        }
+
+        private static int GetStartOf(string sample, int lengthOfStartOf)
+        {
+            for(var i = 0; i < sample.Length - lengthOfStartOf; i++)
+            {
+                var unique = sample
+                    .Substring(i,lengthOfStartOf)
+                    .GroupBy(x => x)
+                    .Any(g => g.Count() > 1);
+                if(!unique) return i + lengthOfStartOf;
+            }
+            return 0;
+        }
+    }
+}
