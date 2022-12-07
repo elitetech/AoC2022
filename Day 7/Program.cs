e29e9d7b46eec7f624e864f2602240a41f88f25d
Koty Bashford <koty.bashford@gmail.com>
2022-12-07 08:21:32 -0500

    Day 7 inprogress

--- Day 7/Program.cs
+++ Day 7/Program.cs
@@ -2,22 +2,63 @@
 {
     class Day_7
     {
+        public static Dictionary<string, Dictionary<string, int>> directoryTree = new();
         public static void Main()
         {
-            var sample = File.ReadAllText("input.txt");
-
-            System.Console.WriteLine($"Part 1: {Part1(sample)}");
-            System.Console.WriteLine($"Part 2: {Part2(sample)}");
+            var input = File.ReadAllLines("input - Copy.txt");
+            SetupTree(input);
+            Console.WriteLine($"Part 1: {Part1(input)}");
+            Console.WriteLine($"Part 2: {Part2(input)}");
         }
 
-        public static int Part1(string sample)
+        public static int Part1(string[] input)
+        {
+            int total = 0;
+            foreach(var directory in directoryTree)
+            {
+                var dirSize = directoryTree
+                        .Where(x => x.Key
+                            .StartsWith(directory.Key))
+                        .Sum(x => x.Value
+                            .Sum(y => y.Value));
+                if (dirSize <= 100000)
+                {
+                    total += dirSize;
+                }
+            }
+            return total;
+        }
+
+        public static int Part2(string[] input)
         {
             return 0;
         }
 
-        public static int Part2(string sample)
+        private static void SetupTree(string[] input)
         {
-            return 0;
+            bool listDir = false;
+            var currentDir = string.Empty;
+            foreach(var item in input)
+            {
+                if (item.Contains('$'))
+                {
+                    if (item.Contains("ls")) listDir = true;
+                    else
+                    {
+                        listDir = false;
+                        var dir = item.Split(' ')[2];
+                        if (dir != "..") currentDir = currentDir.Length == 0 ? dir : $"{currentDir}{dir}/";
+                        else currentDir = currentDir.Substring(0, currentDir.LastIndexOf('/'));
+                    }
+                    continue;
+                }
+                var itemParts = item.Split(' ');
+                if (itemParts[0] != "dir") 
+                {
+                    if (!directoryTree.ContainsKey(currentDir)) directoryTree.Add(currentDir, new());
+                    directoryTree[currentDir].Add(itemParts[1], int.Parse(itemParts[0]));
+                }
+            }
         }
     }
 }
