834809e14d45dc0b690e2784ae297125d71429c7
Koty Bashford <koty.bashford@gmail.com>
2022-12-08 07:36:04 -0500

    Day 8 setup

---
+++ Day 8/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace AoC2022
+{
+    class Day_7
+    {
+        public static Dictionary<string, Dictionary<string, int>> directoryTree = new();
+        public static void Main()
+        {
+            var input = File.ReadAllLines("input.txt");
+            Console.WriteLine($"Part 1: {Part1(input)}");
+            Console.WriteLine($"Part 2: {Part2(input)}");
+        }
+
+        public static int Part1(string[] input)
+        {
+            return 0;
+        }
+        
+        public static int Part2(string[] input)
+        {
+            return 0;
+        }
+    }
+}
