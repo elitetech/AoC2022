2c824f660ee3aebcbd682a38172f9b6c60a78dff
Koty Bashford <koty.bashford@gmail.com>
2022-12-09 07:31:23 -0500

    Day 9 WIP

--- Day 9/Program.cs
+++ Day 9/Program.cs
@@ -2,6 +2,16 @@
 {
     class Day_9
     {
+        enum Direction
+        {
+            Up = 'U',
+            Down = 'D',
+            Left = 'L',
+            Right = 'R'
+        }
+
+        private static Dictionary<int, int> visited = new();
+
         public static void Main()
         {
             var input = File.ReadAllLines("input.txt");
@@ -11,11 +21,30 @@
 
         private static int Part1(string[] input)
         {
+            Head head = new() { x = 0, y = 0 };
+            Tail tail = new() { x = 0, y = 0 };
+            visited.Add(tail.x, tail.y);
+            foreach(var line in input)
+            {
+
+            }
             return 0;
         }
         private static int Part2(string[] input)
         {
             return 0;
         }
+    }
+
+    class Head
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+        
+    class Tail
+    {
+        public int x { get; set; }
+        public int y { get; set; }
     }
 }
